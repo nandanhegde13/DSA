a6ad7013ed85ff4a9449cd250db6a88d94e8aead
Nandan Hegde <55938000+nandanhegde13@users.noreply.github.com>
2022-09-13 05:43:22 +0530

    Create max_consecutive_one.java

---
+++ Arrays/max_consecutive_one.java
@@ -0,0 +1,35 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class Main {
+    static Scanner scan  = new Scanner(System.in);
+    public static void main(String[] args) {
+        int[] arr = new int[7];
+        for(int i=0;i<arr.length;i++){
+            arr[i] = scan.nextInt();
+        }
+
+        System.out.println(max_consecutive_one(arr));
+
+//        for(int i=0;i<arr.length;i++){
+//            System.out.println(arr[i]);
+//        }
+
+    }
+
+  static int max_consecutive_one(int[] arr){
+        int res = 0,count=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i] == 0){
+                count =0;
+            }
+            else{
+                count++;
+                res = Math.max(res,count);
+            }
+        }
+        return res;
+  }
+
+}
